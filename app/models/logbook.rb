6494dfa380608725ca18e76976cc15623b5fd65e
2085099 <2085099M@student.gla.ac.uk>
2016-09-10 20:56:50 +0100

    ICAO identifier force up case

--- app/models/logbook.rb
+++ app/models/logbook.rb
@@ -1,4 +1,12 @@
 class Logbook < ActiveRecord::Base
 	has_many :airports
 	belongs_to :user
+
+
+	before_validation :uppercase_dep_arr
+
+	def uppercase_dep_arr
+		departurePlace.upcase!
+		arrivalPlace.upcase!
+	end
 end
